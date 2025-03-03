2e255aea6008c9ac0b1d43fed797143381963b87
Sergio Aguirre <sergio.m.aguirre@hotmail.com>
2025-03-03 11:11:18 -0600

    Define the data and the DataFrame in main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+import numpy as np
+import pandas as pd
 
+data = np.random.randint(10,21, size=(3,3))
+
+tagColumn = ['Week1','Week2','Week3']
+tagMembers = ["Jim","Dimas","Shon"]
+
+firstTable = pd.DataFrame(data, columns=tagColumn)
+firstTable.index = tagMembers
+print(firstTable)
