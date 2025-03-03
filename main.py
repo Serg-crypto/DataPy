e920511e62b376e91f62c47fa0f82201be0ce264
Sergio Aguirre <sergio.m.aguirre@hotmail.com>
2025-03-03 11:52:48 -0600

    Delete readme file

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
 import numpy as np
 import pandas as pd
 
+
+
+#Información sobre el entorno virtual creado para la ejecución del proyecto;
+
+#crear el entorno virtual: python -m venv venv
+#activar el entorno virtual: venv\Scripts\activate
+#Cambiar el interprete de python CTRL + SHIFT + P
+
+#desactivar el entorno virtual: deactivate
+
+
+
+
+
+
+
 data = np.random.randint(10,21, size=(3,3))
 
 tagColumn = ['Week1','Week2','Week3']
--- readme.md
+++
@@ -1,7 +0,0 @@
-Información sobre el entorno virtual creado para la ejecución del proyecto;
-
-crear el entorno virtual: python -m venv venv
-activar el entorno virtual: venv\Scripts\activate
-Cambiar el interprete de python CTRL + SHIFT + P
-
-desactivar el entorno virtual: deactivate
